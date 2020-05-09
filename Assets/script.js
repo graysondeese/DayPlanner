f0b6864450e633b265b73996cbe6214839e103a9
Grayson Deese <gldeese@ncsu.edu>
2020-05-08 21:03:16 -0400

    Saving the user input to local storage.

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     function startTimer() {
     var currentHour = new Date().getHours();
     $(".description").each(function () {
+        // setting the past, present, and future classes
         if($(this).attr("data-hour") < currentHour) {
             $(this).addClass("past");
         } else if($(this).attr("data-hour") > currentHour) {
@@ -22,4 +23,15 @@ $(document).ready(function(){
     }
 startTimer();
 setInterval(startTimer, 60000);
+
+// setting up local storage for save button
+$(".saveBtn").click(function(){
+var timeKey = $(this).parent().attr("id");
+var valueText = $(this).siblings(".description").val();
+// sending the value to local storage
+localStorage.setItem(timeKey, valueText);
+})
+
+
+
 })
